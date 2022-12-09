22461bdbadd4284199bac7d5f9f1bb24c0973214
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-09 14:19:19 +0530

    localstorage

--- src/main/java/com/example/ShoppingCart_Backend/controller/ShoppingcartController.java
+++ src/main/java/com/example/ShoppingCart_Backend/controller/ShoppingcartController.java
@@ -51,13 +51,37 @@ public class ShoppingcartController {
     @CrossOrigin(origins = "*")
     @PostMapping(path = "/userregistration", consumes = "application/json", produces = "application/json")
     public Map<String, String> UserRegistration(@RequestBody Userregistration u){
-        System.out.println(u.getName().toString());
-        System.out.println(u.getAddress().toString());
-        System.out.println(u.getPhone().toString());
-        System.out.println(u.getEmail().toString());
         dao1.save(u);
         HashMap<String, String> map=new HashMap<>();
         map.put("status","success");
         return map;
+    }
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/userlogin", consumes = "application/json", produces = "application/json")
+    public HashMap<String, String> UserLogin(@RequestBody Userregistration u){
+
+        String username=String.valueOf(u.getEmail());
+        String password=String.valueOf(u.getPassword());
+//        return (List<Userregistration>) dao1.userLogin(u.getEmail(),u.getPassword());
+        List<Userregistration> result=(List<Userregistration>) dao1.userLogin(username, password);
+        HashMap<String,String> st=new HashMap<>();
+        if (result.size()==0)
+        {
+            st.put("status","failed");
+        }
+        else
+        {
+            int id=result.get(0).getId();
+            st.put("userid",String.valueOf(id));
+            st.put("status","success");
+        }
+        return st;
+    }
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/profileview",consumes = "application/json",produces = "application/json")
+    public List<Userregistration> ProfileView(@RequestBody Userregistration u)
+    {
+        return (List<Userregistration>) dao1.profileView(u.getId());
+
     }
 }
--- src/main/java/com/example/ShoppingCart_Backend/dao/UserregistrationDao.java
+++ src/main/java/com/example/ShoppingCart_Backend/dao/UserregistrationDao.java
@@ -1,7 +1,18 @@
 package com.example.ShoppingCart_Backend.dao;
 
 import com.example.ShoppingCart_Backend.model.Userregistration;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserregistrationDao extends CrudRepository<Userregistration,Integer> {
+
+    @Query(value = "SELECT `id`, `address`, `cpassword`, `email`, `name`, `password`, `phone` FROM `registration` WHERE `email` = :email AND `password` = :password",nativeQuery = true)
+    List<Userregistration> userLogin(@Param("email") String email, @Param("password") String password);
+
+    @Query(value = "SELECT `id`, `address`, `cpassword`, `email`, `name`, `password`, `phone` FROM `registration` WHERE `id`=:id",nativeQuery = true)
+    List<Userregistration> profileView(@Param("id") Integer id);
 }
